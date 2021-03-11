1aa2ea2e55c8b2f8d6535f09dc7563acab2fe736
TesyarRAz <mohamadtesyar@gmail.com>
2021-03-11 21:26:35 +0700

    rotasi bangsat musingin

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -1,7 +1,11 @@
 extends "res://src/Base.gd"
 
 func control(delta):
-	$Lodong.look_at(get_global_mouse_position())
+	var angle = get_angle_to(get_global_mouse_position())
+	var otw = $Lodong.get_transform().get_rotation() + angle
+	if otw < deg2rad(180) and otw > deg2rad(0):
+		$Lodong.look_at(get_global_mouse_position())
+	
 	var rotationDirection = 0
 	if Input.is_action_pressed("turn_left"):
 		rotationDirection -= 1
---
+++ src/Ui/MainMenu.gd
@@ -0,0 +1,12 @@
+extends Panel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_btn_exit_pressed():
+	get_tree().quit(0);
+
+
+func _on_btn_new_game_pressed():
+	get_tree().change_scene("res://src/Player/Player.tscn")
