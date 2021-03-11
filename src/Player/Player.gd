8e28168ef6ec178fd36bfb650a4e18a204e2a48f
TesyarRAz <mohamadtesyar@gmail.com>
2021-03-11 21:48:21 +0700

    rotasi bangsat musingin

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -1,10 +1,7 @@
 extends "res://src/Base.gd"
 
 func control(delta):
-	var angle = get_angle_to(get_global_mouse_position())
-	var otw = $Lodong.get_transform().get_rotation() + angle
-	if otw < deg2rad(180) and otw > deg2rad(0):
-		$Lodong.look_at(get_global_mouse_position())
+	control_camera()
 	
 	var rotationDirection = 0
 	if Input.is_action_pressed("turn_left"):
@@ -17,3 +14,10 @@ func control(delta):
 		velocity = Vector2(speed, 0).rotated(rotation)
 	if Input.is_action_pressed("backward"):
 		velocity = Vector2(-speed/2, 0).rotated(rotation)
+
+func control_camera():
+	var mouse = get_global_mouse_position()
+	
+	$Lodong.look_at(mouse)
+	
+	$Lodong.rotation = clamp($Lodong.rotation, deg2rad(0), deg2rad(90))
